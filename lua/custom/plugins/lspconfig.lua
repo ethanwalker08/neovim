1885ff82ab572a828454532b33035c53e11bb2e3
Ethan Walker <ethanwalker23081@gmail.com>
2026-04-03 10:43:01 -0400

    fix(lspconfig): remove Deno server configuration and clean up TypeScript server logic

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -15,7 +15,6 @@ return {
 	},
 	config = function()
 		local util = require("lspconfig.util")
-		local deno_root = util.root_pattern("deno.json", "deno.jsonc")
 		local node_root = util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")
 		local default_server_opts = {
 			flags = {
@@ -56,31 +55,17 @@ return {
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 		local servers = {
-			-- clangd = {},
-			-- Deno language server
-			denols = {
-				root_dir = deno_root,
-				single_file_support = false,
-			},
-			-- TypeScript/JavaScript server for non-Deno projects
+			-- TypeScript/JavaScript server
 			ts_ls = {
 				root_dir = function(fname)
-					if deno_root(fname) then
-						return nil
-					end
 					return node_root(fname)
 				end,
 				single_file_support = false,
 			},
-			-- gopls = {},
-			-- pyright = {},
-			-- rust_analyzer = {},
-			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-			--
-			-- Some languages (like typescript) have entire language plugins that can be useful:
-			--    https://github.com/pmizio/typescript-tools.nvim
-			-- But for many setups, the LSP (`ts_ls`) will work just fine
+			-- Python server
+			pyright = {},
 
+			-- Lua server for working with the Neovim config itself
 			lua_ls = {
 				-- cmd = {...},
 				-- filetypes = { ...},
